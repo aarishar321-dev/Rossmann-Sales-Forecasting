572b534d5d9ced7a3b4c02087df753425bc64c81
Aarish Ahamed M <253781902+aarishar321-dev@users.noreply.github.com>
2026-08-09 21:25:31 +0530

    Test preprocessor loading

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import joblib
 
 st.set_page_config(
     page_title="Rossmann Sales Forecast",
@@ -7,3 +8,13 @@ st.set_page_config(
 
 st.title("Rossmann Sales Forecast")
 st.write("APP STARTED")
+
+@st.cache_resource
+def load_preprocessor():
+    return joblib.load("models/forecast_preprocessor.pkl")
+
+with st.spinner("Loading preprocessor..."):
+    preprocessor = load_preprocessor()
+
+st.success("Preprocessor loaded successfully.")
+st.write(type(preprocessor))
